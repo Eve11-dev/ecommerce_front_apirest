4d998ba8df59245277e0fc52c405f3aef564cf9c
Eve11-dev <evecruz110302@gmail.com>
2023-04-24 13:34:16 -0600

    first commit

--- src/components/nav/Products/ProductsList.tsx
+++ src/components/nav/Products/ProductsList.tsx
@@ -2,6 +2,15 @@ import { getProducts } from "../../../api/products.api";
 
 export const ProductsList = () => {
     const products = getProducts();
+
+    const fetcProducts = async () => {
+        const Products = await getProducts();
+         console.log(products);
+    };
+
+
+
+    fetcProducts()
   return <div>ProductsList</div>
   
 }
