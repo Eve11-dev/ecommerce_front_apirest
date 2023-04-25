8c1715165c2acbce5f602869d7b2f7380adace7d
Eve11-dev <evecruz110302@gmail.com>
2023-04-25 01:47:41 -0600

    importanto nuevo model

--- src/components/nav/Products/ProductsList.tsx
+++ src/components/nav/Products/ProductsList.tsx
@@ -1,16 +1,34 @@
+import { useEffect, useState } from "react";
 import { getProducts } from "../../../api/products.api";
+import { Grid } from "@mui/material";
 
 export const ProductsList = () => {
+
+  const [Products, setProduct] = useState()
     const products = getProducts();
 
-    const fetcProducts = async () => {
+    const fetchProducts = async () => {
         const Products = await getProducts();
-         console.log(products);
+         setProduct(Products);
     };
 
 
+useEffect(() =>{
+  fetchProducts();
+}, [])
 
-    fetcProducts()
-  return <div>ProductsList</div>
+  return 
+  <div>
+    <h1>Products</h1>
+    <Grid container>
+      {products.map((product) => (
+        <Grid item xs={12} sm={6} md= {4} lg={3} key={product.id}>
+          <h2>{product.name} </h2>
+          <p>{product.description} </p>
+          <p>{product.price} </p>
+          </Grid>
+      ))}
+    </Grid>
+  </div>
   
 }
